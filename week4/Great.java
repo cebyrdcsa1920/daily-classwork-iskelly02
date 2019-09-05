9e717475967e5ed36e345f838c1401448cf8f975
iskelly02 <marthacd02@gmail.com>
2019-09-05 14:06:39 +0000

    Add House.java and HouseRunner.java to week4/

---
+++ week4/Great.java
@@ -0,0 +1,7 @@
+public class Great
+{
+    public static void main(String[] args)
+    {
+        private String
+    }
+}
---
+++ week4/House.java
@@ -0,0 +1,27 @@
+public class House
+{
+    private String model;
+    private Color color;
+    private int numBedrooms;
+    private double numBathrooms;
+
+    public House()
+    {
+        model = "Two-Story";
+        color = Color.RED;
+        numBedrooms = 6;
+        numBathrooms = 5.5;
+    }
+
+    public String toString()
+    {
+        return "This is my Two-Story House";
+    }
+}
+
+
+
+
+
+
+
