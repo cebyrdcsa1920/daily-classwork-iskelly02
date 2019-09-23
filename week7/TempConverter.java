463621e834f12680a2767e8822fde7d3be1790df
iskelly02 <marthacd02@gmail.com>
2019-09-23 14:02:32 +0000

    Add ./ to file week7/

---
+++ week7/TempConverter.java
@@ -0,0 +1,16 @@
+public class TempConverter
+{
+    private double temperature;
+    private boolean isCelsius;
+
+    public TempConverter(double celsius)
+    {
+        temperature = celsius;
+        isCelsius = true;
+    }
+
+    public void convertToF()
+    {
+
+    }
+}
